1594ba94b2c7e98cf962fe9957055a9fa0b66702
asif2212 <asifmd2212@gamil.com>
2023-10-12 10:59:34 +0600

    error handler adding

--- mern-backend/controllers/userController.js
+++ mern-backend/controllers/userController.js
@@ -1,17 +1,18 @@
 const User = require("../models/userModel");
-
-const createUser = async (req, res) =>{
+const asyncHandler = require('express-async-handler');
+const createUser = asyncHandler(async (req, res) =>{
     const email = req.body.email
     const findUser = await User.findOne({email:email})
     if(!findUser){
         const newUser = await User.create(req.body)
         res.json(newUser)
     }else{
-        res.json({
-            msg: "User Already Exists",
-            success: false
-        })
+        // res.json({
+        //     msg: "User Already Exists",
+        //     success: false
+        // })
+        throw new Error("User Already Exists")
     }
-}
+});
 
 module.exports = {createUser}
--- mern-backend/index.js
+++ mern-backend/index.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv').config()
 const bodyParser = require("body-parser");
 const express = require('express')
-const dbConnect = require('./config/dbConnect')
+const dbConnect = require('./config/dbConnect');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 authRouter = require("./routes/authRoute");
 const app = express()
 
@@ -16,6 +17,8 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", authRouter)
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`)
 })
